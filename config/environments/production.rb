a4798991db8503ddfbbc48d8ffd6377c6144a74d
Carl Prude <carl.prude@gmail.com>
2014-07-13 13:50:45 -0700

    new initaliser env.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,10 +85,11 @@ Rails.application.configure do
   # Note to set this to actual host name
 
  # Sets Amazon Images
-  config.paperclip_defaults = {
+ 
+config.paperclip_defaults = {
   :storage => :s3,
   :s3_credentials => {
-    :bucket => ENV['S3_BUCKET_NAME'],
+    :bucket => ENV['AWS_BUCKET'],
     :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
     :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
   }
---
+++ config/initializers/env.rb
@@ -0,0 +1,3 @@
+ENV['s3_bucket'] = 'AWS_BUCKET'
+ENV['s3_access_key_id'] = 'AWS_ACCESS_KEY_ID'
+ENV['s3_secret_access_key'] = 'AWS_SECRET_ACCESS_KEY'
